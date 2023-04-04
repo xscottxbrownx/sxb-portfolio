9a1960a0840fd4bdce1f051be7594fd64e5c4a47
veganedge <scottxbrown@yahoo.com>
2023-04-04 16:59:52 -0700

    2 new components to split Projects.js code

---
+++ src/components/Projects/ProjectImageRight.js
@@ -0,0 +1,49 @@
+// Imports from React Animate On Scroll
+import { AnimationOnScroll } from "react-animation-on-scroll";
+// Imported Components
+import ProjectsCard from "../ProjectsCard/ProjectsCard";
+// Imported Stylesheet
+import styles from "./Projects.module.css";
+
+
+
+export default function ProjectImageRight({project}) {
+  const { title, tech, description, imageLink, imageAlt, imageStyle, hrefSourceCode, hrefViewLive } = project;
+
+  return (
+    <div className={`twoColumns ${styles.projectMarginBottom}`}>
+      {/* project description */}
+      <AnimationOnScroll
+        animateIn="animate__fadeInLeft"
+        animateOut="animate__fadeOut"
+        offset={300}
+        duration={0.6}
+      >
+        <div>
+          <ProjectsCard
+            projectTitle={title}
+            projectTech={tech}
+            projectDescription={description}
+            hrefSourceCode={hrefSourceCode}
+            hrefViewLive={hrefViewLive}
+          />
+        </div>
+      </AnimationOnScroll>
+      {/* project image */}
+      <AnimationOnScroll
+        animateIn="animate__fadeInRight"
+        animateOut="animate__fadeOut"
+        offset={300}
+        duration={0.6}
+      >
+        <div>
+          <img
+            src={imageLink}
+            alt={imageAlt}
+            style={imageStyle}
+          />
+        </div>
+      </AnimationOnScroll>
+    </div>
+  );
+}
