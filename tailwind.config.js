8d98724ebf19e411ad71d32ace51d15380278091
veganedge <scottxbrown@yahoo.com>
2023-04-07 10:11:23 -0700

    adding to custom tailwind config for gradients

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,20 +7,19 @@ module.exports = {
     extend: {
       colors: {
         'main': "var(--main)",
+        'mainFrom': "rgba(27,227,227,1)",
+        'mainTo': "rgba(0,212,255,1)", 
         'accent1': "var(--accent1)",
         'accent2': "var(--accent2)",
         'accent3': "var(--accent3)",
         'accent4': "var(--accent4)",
       },
+      gradientColorStopPositions: {
+        '48%': '48%',
+      },
       backgroundImage: {
         'main': 'var(--main)',
-      },
-      transitionDuration: {
-        '2000': '2000ms',
-      },
-      margin: {
-        '21%': '21%',
-      },
+      }, 
     },
   },
   plugins: [],
