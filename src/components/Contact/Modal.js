75b5e682e5c67923382c59661a3839bcdb5d25ae
veganedge <scottxbrown@yahoo.com>
2023-04-09 12:07:12 -0700

    replacing vanilla CSS with Typescript

--- src/components/Contact/Modal.js
+++ src/components/Contact/Modal.js
@@ -1,7 +1,4 @@
-// Style Imports
-// import styles from "./Modal.module.css";
-
-export default function Modal({showModal, setShowModal}) {
+export default function Modal({ showModal, setShowModal }) {
 
   const toggleModal = () => setShowModal(!showModal);
 
@@ -11,19 +8,29 @@ export default function Modal({showModal, setShowModal}) {
     document.body.classList.remove("active-modal");
   }
 
+
   return (
     <>
       {showModal && (
-        <div className="modal">
-          <div onClick={toggleModal} className="overlay"></div>
-          <div className="modal-content">
-            <h2>Contact Form Submitted</h2>
-            <div className="modal-content-paragraph">
-              <p>Thank you for sending your inquiry to <strong>hello@scottxbrown.com</strong>!</p>
+        <div className="w-screen h-screen fixed top-0 right-0 bottom-0 left-0">
+          <div
+            className="w-screen h-screen fixed top-0 right-0 bottom-0 left-0 bg-gray-900/75 cursor-pointer"
+            onClick={toggleModal}
+          />
+          <div className="bg-gray-100 absolute left-[50%] top-[40%] -translate-x-1/2 -translate-y-1/2 p-7 leading-6 rounded max-w-[600px] min-w-[275px]">
+            <h2 className="text-xl font-bold">Contact Form Submitted</h2>
+            <div className="mt-7">
+              <p>
+                Thank you for sending your inquiry to{" "}
+                <strong>hello@scottxbrown.com</strong>!
+              </p>
               <p>You should receive a response within a few business days.</p>
-            </div> 
-            <button className="close-modal" onClick={toggleModal}>
-              CLOSE
+            </div>
+            <button
+              className="absolute top-2.5 right-2.5 py-1 px-2 font-black uppercase"
+              onClick={toggleModal}
+            >
+              X
             </button>
           </div>
         </div>
