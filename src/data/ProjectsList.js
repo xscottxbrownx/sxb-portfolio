fa823b4322b28bbf268716d625e54e8ac40432a4
veganedge <scottxbrown@yahoo.com>
2023-04-09 12:08:34 -0700

    Formatting text of ProjectsList tech list

--- src/data/ProjectsList.js
+++ src/data/ProjectsList.js
@@ -16,7 +16,7 @@ const imageStyle = {
 const projects = [
   {
     title: "FMC Site Mockup",
-    tech: ["NextJS", "AlchemySDK", "FontAwesomeIcons", "API's", "Web3", "NFTs", "Crypto"],
+    tech: ["NextJS", "AlchemySDK", "FontAwesome", "NFTs", "Crypto"],
     description:
       "A mockup site for a company that I co-founded in late September 2021. This company started as a discord server for NFT traders and friends. It quickly grew to over 15k members and 60k twitter followers all organically (no bots.) The server was focused on NFT education and alpha, and as such we also launched our own NFT to token-gate the valuable information in April 2022. This mockup has all information needed about the company and a dashboard area that includes: connection to your web 3 wallet or typed in wallet address, gallery of nfts, token balances, last 30 day history of transactions, floor price and holder informational charts by collection, and some more work-in-progress components.",
     imageLink: project1img,
@@ -38,7 +38,7 @@ const projects = [
   },
   {
     title: "Tickets Bot Documentation",
-    tech: ["Markdown", "mdbook", "webp Images"],
+    tech: ["Markdown", "mdbook", "webpImages"],
     description:
       "I've used this discord bot since February 2022. It was clear their team needed much help with customer support and the documentation on the bot was lacking. I took it upon myself to help their users in the Tickets bot Support discord server. Tickets bot team ultimately saw the value I provided and offered me a spot on their official support team.  Shortly after, I convinced them to allow me to completely re-write the documentation to explain all features, settings, and possible configuration options - as it was obviously needed.",
     imageLink: project3img,
@@ -49,7 +49,7 @@ const projects = [
   },
   {
     title: "Portfolio Site",
-    tech: ["React", "EmailJS", "Animate-On-Scroll", "Tailwind"],
+    tech: ["React", "EmailJS", "AnimateOnScroll", "Tailwind"],
     description:
       "You are currently here. Site to house the current portfolio and resume of Scott Brown. Contact information available as well as links to social media profiles.",
     imageLink: project4img,
