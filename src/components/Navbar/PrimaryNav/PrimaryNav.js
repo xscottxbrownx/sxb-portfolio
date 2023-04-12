6f4ff65ed3cc10652e85196afc1c126f4717f9c3
veganedge <scottxbrown@yahoo.com>
2023-04-12 09:51:06 -0700

    clicking slantedlink will close mobile nav menu

--- src/components/Navbar/PrimaryNav/PrimaryNav.js
+++ src/components/Navbar/PrimaryNav/PrimaryNav.js
@@ -6,48 +6,103 @@ import { MdClose } from "react-icons/md";
 import { IconContext } from "react-icons";
 
 
+
 export default function PrimaryNav({ mobileMenu, setMobileMenu }) {
 
-  const handleClick = () => {
-    setMobileMenu(!mobileMenu);
-  }
-
-
+  const handleClick = () => setMobileMenu(!mobileMenu);
 
 
   return (
-    <>
-      <nav className={mobileMenu ? "hidden items-center justify-start pl-2" : "hidden md:flex items-center justify-start pl-2"}>
-        <SlantedLink href="#skills" text="skills" />
-        <SlantedLink href="#projects" text="projects" />
-        <SlantedLink href="#about" text="about" />
-        <SlantedLink href="#contact" text="contact" />
-      </nav>
-      {/* Mobile menu button */}
-      <div className={mobileMenu ? "flex items-center justify-start pl-2" : "md:hidden flex items-center justify-start pl-2"}>
-        <IconContext.Provider value={{ className: "h-10 w-10 text-black hover:text-accent2" }}>
+    <nav>
+      <div
+        className={
+          mobileMenu
+            ? "hidden"
+            : "hidden md:flex items-center justify-start pl-2"
+        }
+      >
+        <SlantedLink
+          href="#skills"
+          text="skills"
+          mobileMenu={mobileMenu}
+          setMobileMenu={setMobileMenu}
+        />
+        <SlantedLink
+          href="#projects"
+          text="projects"
+          mobileMenu={mobileMenu}
+          setMobileMenu={setMobileMenu}
+        />
+        <SlantedLink
+          href="#about"
+          text="about"
+          mobileMenu={mobileMenu}
+          setMobileMenu={setMobileMenu}
+        />
+        <SlantedLink
+          href="#contact"
+          text="contact"
+          mobileMenu={mobileMenu}
+          setMobileMenu={setMobileMenu}
+        />
+      </div>
+
+      {/* mobile menu button */}
+      <div
+        className={
+          mobileMenu
+            ? "flex items-center justify-start pl-2"
+            : "md:hidden flex items-center justify-start pl-2"
+        }
+      >
+        <IconContext.Provider
+          value={{ className: "h-10 w-10 text-black hover:text-accent2" }}
+        >
           <button className="outline-none" onClick={handleClick}>
             {mobileMenu ? <MdClose /> : <GiHamburgerMenu />}
           </button>
         </IconContext.Provider>
       </div>
+
       {/* mobile menu */}
-      <div className={mobileMenu ? "" : "hidden"}>
-        <ul className="flex flex-col items-center w-screen">
-          <li className="w-80">
-            <SlantedLink href="#skills" text="skills"  />
-          </li>
-          <li className="w-80">
-            <SlantedLink href="#projects" text="projects" />
-          </li>
-          <li className="w-80">
-            <SlantedLink href="#about" text="about" />
-          </li>
-          <li className="w-80">
-            <SlantedLink href="#contact" text="contact" />
-          </li>
-        </ul>
-      </div>
-    </>
+      {mobileMenu && (
+        <div className="flex flex-col items-center w-screen">
+          <ul>
+            <li className="w-80">
+              <SlantedLink
+                href="#skills"
+                text="skills"
+                mobileMenu={mobileMenu}
+                setMobileMenu={setMobileMenu}
+              />
+            </li>
+            <li className="w-80">
+              <SlantedLink
+                href="#projects"
+                text="projects"
+                mobileMenu={mobileMenu}
+                setMobileMenu={setMobileMenu}
+              />
+            </li>
+            <li className="w-80">
+              <SlantedLink
+                href="#about"
+                text="about"
+                mobileMenu={mobileMenu}
+                setMobileMenu={setMobileMenu}
+              />
+            </li>
+            <li className="w-80">
+              <SlantedLink
+                href="#contact"
+                text="contact"
+                mobileMenu={mobileMenu}
+                setMobileMenu={setMobileMenu}
+              />
+            </li>
+          </ul>
+        </div>
+      )}
+    </nav>
   );
 }
--- src/components/Navbar/SlantedLink/SlantedLink.js
+++ src/components/Navbar/SlantedLink/SlantedLink.js
@@ -3,14 +3,30 @@ import styles from "./SlantedLink.module.css";
 
 
 
-export default function SlantedLink({ href, text, target, rel }) {
+export default function SlantedLink({
+  href,
+  text,
+  target,
+  rel,
+  mobileMenu,
+  setMobileMenu,
+}) {
 
+  const handleClick = () => {
+    if (mobileMenu) {
+      setMobileMenu(!mobileMenu);
+    }
+  };
+
+
+  
   return (
     <a
       className={`m-[.35rem] px-4 py-[.40rem] flex justify-center font-bold -skew-x-[38deg] bg-main bg-clip-text text-transparent border-2 group ${styles.slantedLink}`}
       href={href}
       target={target}
       rel={rel}
+      onClick={handleClick}
     >
       <span className="uppercase skew-x-[38deg] bg-main bg-clip-text text-transparent group-hover:text-white">
         {text}
