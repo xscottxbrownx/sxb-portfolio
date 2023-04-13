e77c79d99963047897b31c5d9ce55ec64d147f17
veganedge <scottxbrown@yahoo.com>
2023-04-13 07:26:44 -0700

    splitting CONTACT.js into 2 components

---
+++ src/components/Contact/ContactForm.js
@@ -0,0 +1,77 @@
+// React Imports
+import { useRef } from "react";
+// EmailJS Imports
+import emailjs from "@emailjs/browser";
+// Style Imports
+import styles from "./Contact.module.css";
+
+
+
+export default function ContactForm({ toggleModal }) {
+
+  const form = useRef();
+
+  // handle SEND button
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_sal06dm",
+        "template_9ncakv3",
+        form.current,
+        "IBCPvo5z_s92Qao1v"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.error(error.text);
+        }
+      );
+    toggleModal();
+    e.target.reset();
+  };
+
+
+
+  return (
+    <form
+      className="flex flex-col items-center lg:p-6"
+      ref={form}
+      onSubmit={sendEmail}
+    >
+      <input
+        type="text"
+        name="name"
+        placeholder="Name"
+        required
+        className="w-60 lg:w-96 mb-4 p-2 border border-black rounded font-sans outline-none focus:placeholder:text-black"
+      />
+      <input
+        type="email"
+        name="email"
+        placeholder="Email"
+        required
+        className="w-60 lg:w-96 mb-4 p-2 border border-black rounded font-sans outline-none focus:placeholder:text-black"
+      />
+      <textarea
+        name="message"
+        placeholder="Message"
+        rows="4"
+        required
+        className="w-60 lg:w-96 mb-4 p-2 border border-black rounded font-sans outline-none focus:placeholder:text-black"
+      />
+      <button
+        type="submit"
+        className={`py-[.20rem] lg:py-[.40rem] px-2 lg:px-4 flex font-bold -skew-x-[38deg] bg-main bg-clip-text text-transparent border-2 group ${styles.contactButton}`}
+      >
+        <span
+          className={`uppercase text-xl font-semibold skew-x-[38deg] bg-main bg-clip-text text-transparent group-hover:text-white ${styles.contactButtonContent}`}
+        >
+          send
+        </span>
+      </button>
+    </form>
+  );
+}
---
+++ src/components/Contact/ContactLinks.js
@@ -0,0 +1,33 @@
+// React Icons Imports
+import { IconContext } from "react-icons";
+// Asset Imports
+import { iconLinks } from "../Navbar/PrimaryNav";
+
+
+
+export default function SocialMedia() {
+
+  const contactIconLinks = iconLinks.map((iconLink) => (
+    <a
+      className="flex items-center justify-center mx-1 lg:p-4 hover:text-accent2"
+      href={iconLink.href}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      {iconLink.icon}
+      <h2 className="lg:p-2 font-extrabold text-base lg:text-2xl">
+        {iconLink.name}
+      </h2>
+    </a>
+  ));
+
+
+
+  return (
+    <div className="hidden xs:flex lg:flex-col items-center justify-center mb-12">
+      <IconContext.Provider value={{ className: "h-6 w-6 lg:h-14 lg:w-14 lg:p-2" }}>
+        {contactIconLinks}
+      </IconContext.Provider>
+    </div>
+  );
+}
